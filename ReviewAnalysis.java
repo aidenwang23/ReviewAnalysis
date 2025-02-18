5b3c8ae7f5afffda8c53aad35174a6612a5b63bd
aidenwang23 <148901698+aidenwang23@users.noreply.github.com>
2025-02-18 22:55:02 +0000

    Complete

---
+++ ReviewAnalysis.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+public class ReviewAnalysis {
+    /** All user reviews to be included in this analysis */
+    private Review[] allReviews;
+
+
+    /** Initializes allReviews to contain all the Review objects to be analyzed */
+    public ReviewAnalysis() 
+    {
+        /* implementation not shown */ 
+    }
+
+    public ReviewAnalysis(Review[] reviews)
+    {
+        allReviews = reviews;
+    }
+
+
+    /**
+     * Returns a double representing the average rating of all the Review objects to
+     * be
+     * analyzed, as described in part (a)
+     * Precondition: allReviews contains at least one Review.
+     * No element of allReviews is null.
+     */
+    public double getAverageRating() 
+    {
+        double sum = 0;
+        for (Review r : allReviews) sum+=r.getRating();
+        return sum / allReviews.length;
+    }
+
+
+    /**
+     * Returns an ArrayList of String objects containing formatted versions of
+     * selected user comments, as described in part (b)
+     * Precondition: allReviews contains at least one Review.
+     * No element of allReviews is null.
+     * Postcondition: allReviews is unchanged.
+     */
+    public ArrayList<String> collectComments() 
+    {
+        ArrayList<String> comments = new ArrayList<String>();
+        for(int i = 0; i<allReviews.length; i++){
+            String s = allReviews[i].getComment();
+            if(s.indexOf("!")>=0){
+                String format = i + "-" + s;
+                String lastChar = format.substring(format.length()-1);
+                if(!lastChar.equals("!") && !lastChar.equals(".")) format += ".";
+                comments.add(format);
+            }
+        }
+        return comments;
+    }
+}
